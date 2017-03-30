877fc1351db93f3aee80340deb95b00b20700c29
Stephen McCarthy <sjmccarthy@gmail.com>
2017-03-30 14:36:08 -0700

    Avoid scientific notation for integer constants

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -15,7 +15,7 @@
 class CCoinsViewCache;
 
 /** Default for -blockmaxsize and -blockminsize, which control the range of sizes the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 2E6;
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 2000000;
 static const unsigned int DEFAULT_BLOCK_MIN_SIZE = 0;
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
 static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 50000;
@@ -28,7 +28,7 @@ static const unsigned int MAX_STANDARD_TX_SIGOPS = MAX_BLOCK_SIGOPS/5;
 /** Default for -maxmempool, maximum megabytes of mempool memory usage */
 static const unsigned int DEFAULT_MAX_MEMPOOL_SIZE = 300;
 /** Default for -blocksizeacceptlimit */
-static const int32_t DEFAULT_BLOCK_ACCEPT_SIZE = 3.7e6;
+static const int32_t DEFAULT_BLOCK_ACCEPT_SIZE = 3700000;
 
 /**
  * Standard script verification flags that standard transactions will comply
