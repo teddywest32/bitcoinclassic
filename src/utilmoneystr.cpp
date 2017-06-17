2167ce5caf8f80a55030a8cb628e6ba3260f34d9
vivshaw <hvivianshaw@gmail.com>
2017-06-17 14:10:53 -0400

    update all strings in utilmoneystr.cpp to std::string

--- src/utilmoneystr.cpp
+++ src/utilmoneystr.cpp
@@ -18,7 +18,7 @@ std::string FormatMoney(const CAmount& n)
     std::int64_t n_abs = (n > 0 ? n : -n);
     std::int64_t quotient = n_abs/COIN;
     std::int64_t remainder = n_abs%COIN;
-    string str = strprintf("%d.%08d", quotient, remainder);
+    std::string str = strprintf("%d.%08d", quotient, remainder);
 
     // Right-trim excess zeros before the decimal point:
     int nTrim = 0;
@@ -33,14 +33,14 @@ std::string FormatMoney(const CAmount& n)
 }
 
 
-bool ParseMoney(const string& str, CAmount& nRet)
+bool ParseMoney(const std::string& str, CAmount& nRet)
 {
     return ParseMoney(str.c_str(), nRet);
 }
 
 bool ParseMoney(const char* pszIn, CAmount& nRet)
 {
-    string strWhole;
+    std::string strWhole;
     std::int64_t nUnits = 0;
     const char* p = pszIn;
     while (isspace(*p))
