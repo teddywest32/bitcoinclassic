bd53fd57ab6a4b2c8c7ac512ba844bf380ecc309
vivshaw <hvivianshaw@gmail.com>
2017-06-21 22:08:07 -0400

    update map to std::map in core_write.cpp

--- src/core_write.cpp
+++ src/core_write.cpp
@@ -53,7 +53,7 @@ std::string FormatScript(const CScript& script)
     return ret.substr(0, ret.size() - 1);
 }
 
-const map<unsigned char, std::string> mapSigHashTypes =
+const std::map<unsigned char, std::string> mapSigHashTypes =
     boost::assign::map_list_of
     (static_cast<unsigned char>(SIGHASH_ALL), std::string("ALL"))
     (static_cast<unsigned char>(SIGHASH_ALL|SIGHASH_ANYONECANPAY), std::string("ALL|ANYONECANPAY"))
