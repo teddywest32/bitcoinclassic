016edb5b5092e48bdb7fbb2dc872a618248b2d61
TomZ <tomz@freedommail.ch>
2017-06-22 10:39:38 +0200

    fix whitespace

--- src/core_write.cpp
+++ src/core_write.cpp
@@ -90,7 +90,7 @@ std::string ScriptToAsmStr(const CScript& script, const bool fAttemptSighashDeco
             } else {
                 // the IsUnspendable check makes sure not to try to decode OP_RETURN data that may match the format of a signature
                 if (fAttemptSighashDecode && !script.IsUnspendable()) {
-		    std::string strSigHashDecode;
+                    std::string strSigHashDecode;
                     // goal: only attempt to decode a defined sighash type from data that looks like a signature within a scriptSig.
                     // this won't decode correctly formatted public keys in Pubkey or Multisig scripts due to
                     // the restrictions on the pubkey formats (see IsCompressedOrUncompressedPubKey) being incongruous with the
