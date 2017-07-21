86e84defda401c0f3e18c623547ee2bbf09bf2a8
ftrader <freetrader@tuta.io>
2017-07-21 13:08:26 +0000

    Use forcednsseed by default prior to the fork
    Summary:
    Ahead of the fork, UAHF nodes are a small minority on the network
    and it is possible that they are not found.
    This guarantees that the ABC seeds will be asked, increasing the
    chance of connecting to good peers.
    
    Can be disabled later once fork peers are easily found without it.
    
    Test Plan:
    Repeatedly:
    - stop node, remove peers.dat
    - start node
    - check that ABC peers are connected to via seeders after startup
      (using getpeerinfo RPC or GUI)
    
    A newly started node should be able to connect to ABC peers very
    rapidly.
    
    Reviewers: #bitcoin_abc, deadalnix
    
    Reviewed By: #bitcoin_abc, deadalnix
    
    Differential Revision: https://reviews.bitcoinabc.org/D360

--- src/net.h
+++ src/net.h
@@ -69,7 +69,10 @@ static const uint64_t DEFAULT_MAX_UPLOAD_TARGET = 0;
 /** Default for blocks only*/
 static const bool DEFAULT_BLOCKSONLY = false;
 
-static const bool DEFAULT_FORCEDNSSEED = false;
+// Force DNS seed use ahead of UAHF fork, to ensure peers are found
+// as long as seeders are working.
+// TODO: Change this back to false after the forked network is stable.
+static const bool DEFAULT_FORCEDNSSEED = true;
 static const size_t DEFAULT_MAXRECEIVEBUFFER = 5 * 1000;
 static const size_t DEFAULT_MAXSENDBUFFER    = 1 * 1000;
 
